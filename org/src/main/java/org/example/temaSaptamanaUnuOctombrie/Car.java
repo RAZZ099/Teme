3a57249c62de4bef21a39e2b8138faec5ab314a6
razz099 <ionut.pruteanu@outlook.com>
2025-10-18 13:40:48 +0300

    Am adaugat gettere si settere pentru proprietatea make precum si constructor care le initializeaza

--- org/src/main/java/org/example/temaSaptamanaUnuOctombrie/Car.java
+++ org/src/main/java/org/example/temaSaptamanaUnuOctombrie/Car.java
@@ -2,4 +2,16 @@ package org.example.temaSaptamanaUnuOctombrie;
 
 public class Car {
     private String make;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public Car(String make) {
+        this.make = make;
+    }
 }
